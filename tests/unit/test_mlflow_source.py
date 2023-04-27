ab84c1a06e33e646e88fc5b8fb849085991147ae
hariishaa <rulezzzz08@gmail.com>
2023-04-27 16:57:06 +0300

    Add more unit tests

--- tests/unit/test_mlflow_source.py
+++ tests/unit/test_mlflow_source.py
@@ -1,10 +1,15 @@
 import datetime
+from typing import TypeVar
 
 import pytest
 from datahub.ingestion.api.common import PipelineContext
+from mlflow.entities.model_registry import RegisteredModel
 from mlflow.entities.model_registry.model_version import ModelVersion
+from mlflow.store.entities import PagedList
 
 from source.mlflow import MLflowSource, MLflowConfig
+
+T = TypeVar('T')
 
 
 @pytest.fixture
@@ -12,18 +17,55 @@ def tracking_uri(tmp_path) -> str:
     return str(tmp_path / "mlruns")
 
 
-def test_stages(tracking_uri):
+@pytest.fixture
+def source(tracking_uri: str) -> MLflowSource:
+    return MLflowSource(
+        ctx=PipelineContext(run_id="mlflow-source-test"),
+        config=MLflowConfig(tracking_uri=tracking_uri),
+    )
+
+
+@pytest.fixture
+def registered_model(source: MLflowSource) -> RegisteredModel:
+    model_name = "abc"
+    return RegisteredModel(name=model_name)
+
+
+@pytest.fixture
+def model_version(source: MLflowSource, registered_model: RegisteredModel) -> ModelVersion:
+    version = "1"
+    return ModelVersion(
+        name=registered_model.name,
+        version=version,
+        creation_timestamp=datetime.datetime.now(),
+    )
+
+
+def dummy_search_func(page_token, **kwargs) -> PagedList[T]:
+    dummy_pages = dict(
+        page_1=PagedList(items=["a", "b"], token="page_2"),
+        page_2=PagedList(items=["c", "d"], token="page_3"),
+        page_3=PagedList(items=["e"], token=None),
+    )
+    if page_token is None:
+        page_to_return = dummy_pages["page_1"]
+    else:
+        page_to_return = dummy_pages[page_token]
+    if kwargs.get("case", "") == "upper":
+        page_to_return = PagedList(
+            items=[e.upper() for e in page_to_return.to_list()],
+            token=page_to_return.token,
+        )
+    return page_to_return
+
+
+def test_stages(source):
     mlflow_registered_model_stages = {
         "Production",
         "Staging",
         "Archived",
         None,
     }
-    source = MLflowSource(
-        ctx=PipelineContext(run_id="mlflow-source-test"),
-        config=MLflowConfig(tracking_uri=tracking_uri),
-    )
-
     workunits = source._get_mlflow_model_registry_stage_workunits()
     names = [wu.get_metadata()["metadata"].aspect.name for wu in workunits]
 
@@ -31,25 +73,41 @@ def test_stages(tracking_uri):
     assert set(names) == {"mlflow_" + str(stage).lower() for stage in mlflow_registered_model_stages}
 
 
-def test_separator(tracking_uri):
-    model_name = "abc"
-    version = "1"
+def test_separator(source, model_version):
     name_version_sep = "+"
-    source = MLflowSource(
-        ctx=PipelineContext(run_id="mlflow-source-test"),
-        config=MLflowConfig(
-            tracking_uri=tracking_uri,
-            model_name_separator=name_version_sep,
-        ),
-    )
-    expected_urn = \
-        f"urn:li:mlModel:(urn:li:dataPlatform:mlflow,{model_name}{name_version_sep}{version},{source.config.env})"
-    model_version = ModelVersion(
-        name=model_name,
-        version=version,
-        creation_timestamp=datetime.datetime.now(),
-    )
+    source.config.model_name_separator = name_version_sep
+    expected_model_name = f"{model_version.name}{name_version_sep}{model_version.version}"
+    expected_urn = f"urn:li:mlModel:(urn:li:dataPlatform:mlflow,{expected_model_name},{source.config.env})"
 
     urn = source._make_ml_model_urn(model_version)
 
     assert urn == expected_urn
+
+
+def test_model_without_run(source, registered_model, model_version):
+    run = source._get_mlflow_run(model_version)
+    wu = source._get_ml_model_properties_workunit(
+        registered_model=registered_model,
+        model_version=model_version,
+        run=run,
+    )
+    aspect = wu.get_metadata()["metadata"].aspect
+
+    assert aspect.hyperParams is None
+    assert aspect.trainingMetrics is None
+
+
+def test_traverse_mlflow_search_func(source):
+    expected_items = ["a", "b", "c", "d", "e"]
+
+    items = list(source._traverse_mlflow_search_func(dummy_search_func))
+
+    assert items == expected_items
+
+
+def test_traverse_mlflow_search_func_with_kwargs(source):
+    expected_items = ["A", "B", "C", "D", "E"]
+
+    items = list(source._traverse_mlflow_search_func(dummy_search_func, case="upper"))
+
+    assert items == expected_items
